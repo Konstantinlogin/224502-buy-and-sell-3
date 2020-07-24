ba084c715585ab3b7b07c2a496c571cf8a2dd58c
konstantin_elagin <k.elagin@xsquare.ru>
2020-07-24 18:47:56 +0300

    generate form txt

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -16,6 +16,10 @@ const {
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
 
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
+
 const OfferType = {
   offer: `offer`,
   sale: `sale`
@@ -31,37 +35,43 @@ const PictureRestrict = {
   max: 16
 };
 
-const generateOffers = (count) => (
+const generateOffers = (count, titles, categories, sentences) => (
   Array(count).fill({}).map(() => ({
-    category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
-    description: shuffle(SENTENCES).slice(1, 5).join(` `),
+    category: [categories[getRandomInt(0, categories.length - 1)]],
+    description: shuffle(sentences).slice(1, 5).join(` `),
     picture: getPictureFileName(getRandomInt(PictureRestrict.min, PictureRestrict.max)),
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
     sum: getRandomInt(SumRestrict.min, SumRestrict.max),
   }))
 );
 
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.split(`\n`);
+  } catch (e) {
+    console.error(chalk.red(e));
+    process.exit(ExitCode.fail);
+  }
+};
+
 module.exports = {
   name: `--generate`,
   async run(count) {
-    const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    if (count > 1000) {
-      console.error(chalk.red(`Не больше 1000 объявлений`));
-      process.exit(ExitCode.fail);
-    }
-    let content;
     try {
-      content = JSON.stringify(generateOffers(countOffer))
-    } catch (e) {
-      console.error(chalk.red(`Can't stringify an object...`));
-      process.exit(ExitCode.fail);
-    }
-    try {
+      if (count > 1000) {
+        throw new Error(`Не больше 1000 объявлений`)
+      }
+      const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
+      const sentences = await readContent(FILE_SENTENCES_PATH);
+      const titles = await readContent(FILE_TITLES_PATH);
+      const categories = await readContent(FILE_CATEGORIES_PATH);
+      const content = JSON.stringify(generateOffers(countOffer, titles, categories, sentences));
       await fs.writeFile(FILE_NAME, content);
       console.log(chalk.green(`Operation success. File created.`));
     } catch (e) {
-      console.error(chalk.red(`Can't write data to file...`));
+      console.error(chalk.red(e));
       process.exit(ExitCode.fail);
     }
   }
